6f4c5903306d87eb551b27477250799c61ba85b7
Nathan <naththth@email.arizona.edu>
2016-08-29 16:43:55 -0700

    we got somewhere

--- update-cloud-config.sh
+++ update-cloud-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 echo "The script ran!"
-bosh target 192.168.50.4
+bosh -n target 192.168.50.4
 bosh login admin admin
 bosh update cloud-config templates/cloud_config.yml
 bosh cloud-config
